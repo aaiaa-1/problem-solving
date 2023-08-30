316ae079198c8c0c6c1744a7b5c01bbe242f1ea7
Aya Hamza <117121490+aaiaa-1@users.noreply.github.com>
2023-08-31 00:45:18 +0100

    add two numbers

---
+++ addTwoNumbers.java
@@ -0,0 +1,49 @@
+/**
+    You are given two non-empty linked lists representing two non-negative integers. 
+    The digits are stored in reverse order, and each of their nodes contains a single digit. 
+    Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example:
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+*/
+
+
+//   Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode(0); // dummy is a common programming technique used in linked list manipulation. 
+                //It's a placeholder or sentinel node that serves as the starting point of a new linked list. 
+                // It's especially useful when you're building a new linked list while iterating through another linked list, as it simplifies the process of linking nodes together
+        ListNode current = dummy;  
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+
+        return dummy.next; // Skip the dummy node
+    }
+}
