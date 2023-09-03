8cfe752736490cb038a94a0b181822e4b34c9bc0
Aya Hamza <117121490+aaiaa-1@users.noreply.github.com>
2023-09-03 22:23:01 +0100

    Linked List Cycle

---
+++ linkedListCycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false; // No cycle if there are less than 2 nodes
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false; // Reached the end, no cycle
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return true; // Cycle detected
+    }
+}
